bc34f802be06c35687926ca54a23a36c99ed060e
mrkmx <alex.a.maximow@gmail.com>
2020-12-02 11:05:31 +0400

    add area of a triangle script

---
+++ lesson_01/area_of_a_triangle.rb
@@ -0,0 +1,8 @@
+puts "Enter the base of a triangle"
+base = Float(gets.chomp)
+
+puts "Enter the height of a triangle"
+height = Float(gets.chomp)
+
+area = 0.5 * base * height
+puts "Area of the triangle is #{area}"

4b7f1baafbf4910d8744233eeff1ec77f74d2a1c
mrkmx <alex.a.maximow@gmail.com>
2020-12-08 12:45:49 +0400

    Update 03_fibonacci

--- lesson_02/03_fibonacci.rb
+++ lesson_02/03_fibonacci.rb
@@ -1,14 +1,8 @@
 # without recursion
-arr = []
-current_int = 0
-next_int = 1
+arr = [0, 1]
 
-loop do
-  tmp = current_int
-  current_int = next_int
-  next_int = tmp + next_int
-  break if current_int >= 100
-  arr << current_int 
+while arr[-2] + arr[-1] < 100 do
+  arr << arr[-2] + arr[-1]
 end
 
 print arr

c3e0414a51d6b2c60478b860603393036bcc96f0
mrkmx <alex.a.maximow@gmail.com>
2021-01-14 12:10:22 +0400

    update instance_counter

--- lesson_05/instance_counter.rb
+++ lesson_05/instance_counter.rb
@@ -5,18 +5,15 @@ module InstanceCounter
   end
 
   module ClassMethods
-    @@instances = 0
-
-    def instances
-      @@instances
-    end
+    attr_accessor :instances
   end
 
   module InstanceMethods
     protected
 
     def register_instance
-      @@instances += 1
+      self.class.instances ||= 0
+      self.class.instances += 1
     end
   end
 end

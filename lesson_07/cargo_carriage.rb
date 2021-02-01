5dc0dbe51c65487d0f847410068fcafce52b3b75
mrkmx <alex.a.maximow@gmail.com>
2021-02-01 10:18:58 +0400

    update carriages

--- lesson_07/cargo_carriage.rb
+++ lesson_07/cargo_carriage.rb
@@ -1,7 +1,20 @@
 require_relative 'carriage'
 
+attr_reader :loaded, :volume
+
 class CargoCarriage < Carriage
-  def initialize
+  def initialize(volume)
     super(:cargo)
+    @volume = volume
+    @loaded = 0
+  end
+
+  def take_volume(vol)
+    raise "not enough space" if vol + @loaded > @volume
+    @loaded += vol
+  end
+
+  def free_volume?
+    @volume - @loaded
   end
 end
--- lesson_07/passenger_carriage.rb
+++ lesson_07/passenger_carriage.rb
@@ -1,7 +1,20 @@
 require_relative 'carriage'
 
+attr_reader :taken_seats, :seats
+
 class PassengerCarriage < Carriage
-  def initialize
+  def initialize(seats)
     super(:passenger)
+    @seats = seats
+    @taken_seats = 0
+  end
+
+  def take_seat
+    raise "all seats are taken" unless @taken_seats == @seats
+    @taken_seats += 1
+  end
+
+  def free_seats?
+    @seats - taken_seats
   end
 end

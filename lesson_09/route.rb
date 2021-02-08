e452c040ce8ce3986fd07d12278c8a437143ac5f
mrkmx <alex.a.maximow@gmail.com>
2021-02-09 00:15:07 +0400

    copy files

---
+++ lesson_09/route.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require_relative 'instance_counter'
+require_relative 'valid'
+
+class Route
+  include InstanceCounter
+  include Valid
+
+  attr_reader :stations
+
+  def initialize(from, to)
+    @from = from
+    @to = to
+    @stations = [@from, @to]
+    register_instance
+    validate!
+  end
+
+  def add_station(station)
+    @stations.insert(-2, station)
+  end
+
+  def remove_station(station)
+    @stations.delete(station) unless [@from, @to].include?(station)
+  end
+
+  private
+
+  def validate!
+    raise 'You must set up a departure station' if from.nil?
+    raise 'You must set up an arrival station' if to.nil?
+  end
+end
